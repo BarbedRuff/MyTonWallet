efd7815de7ccb2085477f852f68bb2943b632a6b
BarbedRuff <Barbed.Ruff@yandex.ru>
2024-08-26 15:41:26 +0300

    first commit

---
+++ app/src/main/java/com/genss/mytonwallet/presentation/MainActivity.kt
@@ -0,0 +1,25 @@
+package com.genss.mytonwallet.presentation
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.ui.Modifier
+import com.genss.mytonwallet.ui.theme.MyTonWalletTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            MyTonWalletTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    innerPadding
+                }
+            }
+        }
+    }
+}
